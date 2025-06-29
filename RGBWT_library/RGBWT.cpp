b2aa0c64f4214428cd7c28a1308879add3b2ed5e
Keith Bond <keithbond94@gmail.com>
2025-06-29 12:02:52 +0100

    removed weather and colourref classes/headers, converted to struct

--- RGBWT_library/RGBWT.cpp
+++ RGBWT_library/RGBWT.cpp
@@ -1,20 +1,9 @@
-#include <Arduino.h>
 #include "Arduino.h"
-#include "RGBWT_WiFi.h"
 #include "RGBWT.h"
 
 RGBWT::RGBWT() {
-
-}
-
-void RGBWT::wifi(const char *usr, const char *psd, const char *ssid, void (*function)()){
-  wifi(usr, psd, ssid, function);
-}
-void RGBWT::wifi(const char *ssid, const char *psd, void (*function)()){
-  wifi(ssid, psd, function);
-}
-void RGBWT::http(String a){
-  http(a);
+  colourInit();
+  weatherInit();
 }
 
 void RGBWT::input(int i,int m, int x, int y){
@@ -27,4 +16,33 @@ void RGBWT::startup(){
   //run menu
   //get map
   //get weather
+}
+
+void RGBWT::weatherInit(){
+  setWeather(rain, "Rain", 532, 300, colour.Red);
+  setWeather(thunder, "Thunder", 233, 200, colour.Purple);
+  setWeather(snow, "Snow", 623, 600, colour.White);
+  setWeather(cloud, "Clouds", 805, 801, colour.Grey);
+}
+
+void RGBWT::setWeather(weather w, String n, int ma, int mi, uint16_t c){
+  w.displayName = n;
+  w.idMax = ma;
+  w.idMin = mi;
+  w.displayColour = c;
+}
+
+void RGBWT::colourInit(){
+  colour.Red = getColour(red[0],red[1],red[2]);
+  colour.Green = getColour(green[0],green[1],green[2]);
+  colour.Blue = getColour(blue[0],blue[1],blue[2]);
+  colour.White = getColour(50,50,50);
+  colour.Purple = getColour(50,blue[1],50);
+  colour.Yellow = getColour(50,50,red[2]);
+  colour.Grey = getColour(255,140,0);
+  colour.Off = getColour(0,0,0);
+}
+
+uint16_t RGBWT::getColour(uint8_t red, uint8_t green, uint8_t blue) {
+  return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3);
 }
--- RGBWT_library/RGBWT.h
+++ RGBWT_library/RGBWT.h
@@ -1,26 +1,41 @@
 #ifndef RGBWT_h
 #define RGBWT_h
-#include <Arduino.h>
 #include "Arduino.h"
-#include "RGBWT_WiFi.h"
-#include "RGBWT_http.h"
-#include "RGBWT_input.h"
-#include "map.h"
+//#include "map.h"
 #include "RGBWT_matrix.h"
-#include "RGBWT_screen.h"
-#include "weatherTypes.h"
 //#include <WiFi.h>
 //#include "esp_wpa2.h"
 
 class RGBWT{
   public:
-    //RGBWT(const char *usr, const char *psd, const char *ssid);
-    //RGBWT(const char *ssid, const char *psd)
-    RGBWT_WiFi wifi();
-    RGBWT_http http();
-    RGBWT_input input();
+    uint8_t red[3] = {25, 0, 0};
+    uint8_t blue[3] = {0, 0, 25};
+    uint8_t green[3] = {0, 25, 0};
+    typedef struct weather{
+		  String displayName = "";
+      int idMax;
+      int idMin;
+      uint16_t displayColour;
+    } weather;
+    weather rain;
+    weather thunder;
+    weather snow;
+    weather cloud;
+    weather current;
+
+    typedef struct colours{
+      uint16_t Red;
+      uint16_t Green;
+      uint16_t Blue;
+      uint16_t White;
+      uint16_t Purple;
+      uint16_t Yellow;
+      uint16_t Grey;
+      uint16_t Off;
+    } colours;
+    colours colour;
+
     //RGBWT_matrix matrix();
-    RGBWT_screen screen();
     RGBWT();
     void wifi(const char *usr, const char *psd, const char *ssid, void (*)());
     void wifi(const char *ssid, const char *psd, void (*)());
@@ -28,6 +43,10 @@ class RGBWT{
     void input(int i,int m, int x, int y);
     void track();
     void startup();
+    void weatherInit();
+    void colourInit();
+    uint16_t getColour(uint8_t red, uint8_t green, uint8_t blue);
+    void setWeather(weather w, String n, int ma, int mi, uint16_t c);
 
 };
 
